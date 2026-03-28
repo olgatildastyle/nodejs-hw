36cbc230e64dd325103f8b980b9b90b53a6ac1f5
Olga Demchenko <olga.tildastyle@gmail.com>
2026-03-28 21:43:47 +0200

    finish mongodb hw

--- src/server.js
+++ src/server.js
@@ -1,63 +1,32 @@
 import express from 'express';
 import cors from 'cors';
-import pino from 'pino-http';
+import helmet from 'helmet';
 import 'dotenv/config';
+
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
 // middleware
-app.use(express.json());
+app.use(logger);
 app.use(cors());
-app.use(
-  pino({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat:
-          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
-  }),
-);
+app.use(helmet());
+app.use(express.json());
 
-// GET /notes
-app.get('/notes', (req, res) => {
-  res.status(200).json({ message: 'Retrieved all notes' });
-});
+app.use(notesRoutes);
 
-// GET /notes/:noteId
-app.get('/notes/:noteId', (req, res) => {
-  const { noteId } = req.params;
-  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
-});
+app.use(notFoundHandler);
+app.use(errorHandler);
 
-app.get('/test-error', () => {
-  throw new Error('Simulated server error');
-});
+// connecting to MongoDB
+await connectMongoDB();
 
-// 404
-app.use((req, res) => {
-  res.status(404).json({ message: 'Route not found' });
-});
-
-// error middleware
-app.use((err, req, res, next) => {
-  const isProd = process.env.NODE_ENV === 'production';
-
-  res.status(500).json({
-    message: isProd
-      ? 'Something went wrong. Please try again later.'
-      : err.message,
-  });
-});
-
-//starting the server
+// starting the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
