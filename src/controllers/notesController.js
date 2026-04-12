7df9a15cdda21087bb9ddb5ac56f7b6d7033b59f
Olga Demchenko <olga.tildastyle@gmail.com>
2026-04-12 22:09:13 +0300

    add auth: register, login, logout, sessions

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -5,8 +5,7 @@ import { Note } from '../models/note.js';
 export const getAllNotes = async (req, res) => {
   const { page = 1, perPage = 10, tag, search } = req.query;
   const skip = (page - 1) * perPage;
-
-  const notesQuery = Note.find();
+  const notesQuery = Note.find({ userId: req.user._id });
 
   if (search) {
     notesQuery.where({ $text: { $search: search } });
@@ -33,37 +32,53 @@ export const getAllNotes = async (req, res) => {
 
 export const getNoteById = async (req, res) => {
   const { noteId } = req.params;
-  const note = await Note.findById(noteId);
 
-  if (!note) {
-    throw createHttpError(404, 'Note not found');
-  }
-  res.status(200).json(note);
-};
-
-export const createNote = async (req, res) => {
-  const note = await Note.create(req.body);
-  res.status(201).json(note);
-};
-
-export const deleteNote = async (req, res) => {
-  const { noteId } = req.params;
-  const note = await Note.findOneAndDelete({ _id: noteId });
-
-  if (!note) {
-    throw createHttpError(404, 'Note not found');
-  }
-  res.status(200).json(note);
-};
-
-export const updateNote = async (req, res) => {
-  const { noteId } = req.params;
-  const note = await Note.findOneAndUpdate({ _id: noteId }, req.body, {
-    returnDocument: 'after',
+  const note = await Note.findOne({
+    _id: noteId,
+    userId: req.user._id,
   });
 
   if (!note) {
     throw createHttpError(404, 'Note not found');
   }
+
+  res.status(200).json(note);
+};
+
+export const createNote = async (req, res) => {
+  const note = await Note.create({
+    ...req.body,
+    userId: req.user._id,
+  });
+  res.status(201).json(note);
+};
+
+export const deleteNote = async (req, res) => {
+  const { noteId } = req.params;
+  const note = await Note.findOneAndDelete({
+    _id: noteId,
+    userId: req.user._id,
+  });
+
+  if (!note) {
+    throw createHttpError(404, 'Note not found');
+  }
+
+  res.status(200).json(note);
+};
+
+export const updateNote = async (req, res) => {
+  const { noteId } = req.params;
+
+  const note = await Note.findOneAndUpdate(
+    { _id: noteId, userId: req.user._id },
+    req.body,
+    { returnDocument: 'after' },
+  );
+
+  if (!note) {
+    throw createHttpError(404, 'Note not found');
+  }
+
   res.status(200).json(note);
 };
--- src/server.js
+++ src/server.js
@@ -3,12 +3,14 @@ import cors from 'cors';
 import helmet from 'helmet';
 import { errors } from 'celebrate';
 import 'dotenv/config';
+import cookieParser from 'cookie-parser';
 
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -18,8 +20,10 @@ app.use(logger);
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
+app.use(cookieParser());
 
 app.use(notesRoutes);
+app.use(authRoutes);
 
 app.use(notFoundHandler);
 app.use(errors());
