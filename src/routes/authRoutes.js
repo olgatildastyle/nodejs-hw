b9bd05df84a773c39252dc6aa0be7fa0925c18e3
Olga Demchenko <olga.tildastyle@gmail.com>
2026-04-13 22:32:38 +0300

    feat: add reset password and avatar upload

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,15 +2,18 @@ import { Router } from 'express';
 import { celebrate } from 'celebrate';
 
 import {
+  registerUser,
   loginUser,
   logoutUser,
   refreshUserSession,
-  registerUser,
+  requestResetEmail,
+  resetPassword,
 } from '../controllers/authController.js';
-
 import {
-  loginUserSchema,
   registerUserSchema,
+  loginUserSchema,
+  requestResetEmailSchema,
+  resetPasswordSchema,
 } from '../validations/authValidation.js';
 
 const router = Router();
@@ -19,5 +22,17 @@ router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 router.post('/auth/logout', logoutUser);
 router.post('/auth/refresh', refreshUserSession);
+
+router.post(
+  '/auth/request-reset-email',
+  celebrate(requestResetEmailSchema),
+  requestResetEmail,
+);
+
+router.post(
+  '/auth/reset-password',
+  celebrate(resetPasswordSchema),
+  resetPassword,
+);
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -11,6 +11,7 @@ import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
 import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -24,6 +25,7 @@ app.use(cookieParser());
 
 app.use(notesRoutes);
 app.use(authRoutes);
+app.use(userRoutes);
 
 app.use(notFoundHandler);
 app.use(errors());
