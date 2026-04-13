1560475296459a88984ec554850d8e3c0e9a752b
Olga Demchenko <olga.tildastyle@gmail.com>
2026-04-13 23:07:38 +0300

    fix

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -8,7 +8,7 @@ import fs from 'node:fs/promises';
 import { User } from '../models/user.js';
 import { Session } from '../models/session.js';
 import { createSession, setSessionCookies } from '../services/auth.js';
-import { sendEmail } from '../utils/sendEmail.js';
+import { sendEmail } from '../utils/sendMail.js';
 
 export const registerUser = async (req, res) => {
   const { email, password } = req.body;
